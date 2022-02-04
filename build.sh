a7b4fd22b5c03afee0dc212b4377ca79acacaced
Kuntai Du <kuntai@aa001.ai.cs.uchicago.edu>
2022-02-04 02:48:43 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
+directory=$(pwd)
 cd x264
-./configure --enable-shared --disable-asm --prefix=/tank/kuntai/
+./configure --enable-shared --disable-asm --prefix=$directory
 make
 make install
-export PATH=$PATH:/tank/kuntai/myh264/x264
+export PATH=$PATH:$directory/x264
 cd ../ffmpeg-3.4.8
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/tank/kuntai/myh264/x264
-./configure --enable-libx264 --enable-gpl --enable-shared --disable-x86asm --prefix=/tank/kuntai/ --extra-cflags="-I/tank/kuntai/myh264/x264" --extra-ldflags="-I/tank/kuntai/myh264/x264"
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$directory/x264
+./configure --enable-libx264 --enable-gpl --enable-shared --disable-x86asm --prefix=$directory --extra-cflags="-I$directory/x264" --extra-ldflags="-I$directory/x264"
 make
 make install
 cd ..
